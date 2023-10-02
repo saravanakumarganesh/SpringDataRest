612852d8aaa7b952af0061f05ce955e77be3cfeb
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2023-10-02 15:15:02 -0400

    rename package

--- src/main/java/com/example/demo/dao/AlienRepository.java
+++ src/main/java/com/example/demo/dao/AlienRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.doa;
+package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
